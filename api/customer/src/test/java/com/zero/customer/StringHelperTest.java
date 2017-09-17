e00f6b2118edd364384688ae96ae2dda00382b27
hiyzx <452002276@qq.com>
2017-09-17 23:30:52 +0800

    add jasypt to encrypt mysql DB password

---
+++ api/customer/src/test/java/com/zero/customer/StringHelperTest.java
@@ -0,0 +1,27 @@
+package com.zero.customer;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * @author yezhaoxing
+ * @date 2017/09/16
+ */
+@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+public class StringHelperTest {
+
+    @Resource
+    private StringEncryptor stringEncryptor;
+
+    @Test
+    public void encryptPwd() {
+        String result = stringEncryptor.encrypt("root1");
+        System.out.println(result);
+    }
+}
