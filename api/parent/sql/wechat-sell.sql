82bc492affc7b16daadc9384ff10fcda64d36824
hiyzx <452002276@qq.com>
2017-09-25 23:38:01 +0800

    upload new sql script

--- api/parent/sql/wechat-sell.sql
+++ api/parent/sql/wechat-sell.sql
@@ -2,15 +2,15 @@
 Navicat MySQL Data Transfer
 
 Source Server         : local
-Source Server Version : 50525
+Source Server Version : 50719
 Source Host           : localhost:3306
 Source Database       : wechat-sell
 
 Target Server Type    : MYSQL
-Target Server Version : 50525
+Target Server Version : 50719
 File Encoding         : 65001
 
-Date: 2017-09-19 17:41:20
+Date: 2017-09-25 23:36:59
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -34,10 +34,6 @@ CREATE TABLE `order_detail` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订单详情';
 
 -- ----------------------------
--- Records of order_detail
--- ----------------------------
-
--- ----------------------------
 -- Table structure for order_master
 -- ----------------------------
 DROP TABLE IF EXISTS `order_master`;
@@ -46,19 +42,15 @@ CREATE TABLE `order_master` (
   `buyer_name` varchar(32) NOT NULL COMMENT '买家名字',
   `buyer_phone` varchar(32) NOT NULL COMMENT '买家电话',
   `buyer_address` varchar(128) NOT NULL COMMENT '买家地址',
-  `buyer_openid` varchar(64) NOT NULL COMMENT '买家唯一id',
+  `buyer_id` varchar(64) NOT NULL COMMENT '买家唯一id',
   `order_amount` double(8,2) NOT NULL COMMENT '订单总金额',
   `order_status` int(3) NOT NULL DEFAULT '0' COMMENT '订单状态 0新下单',
   `pay_status` int(3) NOT NULL DEFAULT '0' COMMENT '支付状态,0未支付',
   `create_time` datetime NOT NULL,
   `update_time` datetime NOT NULL,
   PRIMARY KEY (`order_id`),
-  UNIQUE KEY `idx_buyer_openid` (`buyer_openid`) USING BTREE
+  UNIQUE KEY `idx_buyer_openid` (`buyer_id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订单主表';
-
--- ----------------------------
--- Records of order_master
--- ----------------------------
 
 -- ----------------------------
 -- Table structure for product_category
@@ -75,12 +67,6 @@ CREATE TABLE `product_category` (
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COMMENT='商品类目表';
 
 -- ----------------------------
--- Records of product_category
--- ----------------------------
-INSERT INTO `product_category` VALUES ('1', '豆制品', '1', '2017-09-18 10:05:07', '2017-09-18 10:05:10');
-INSERT INTO `product_category` VALUES ('2', '快餐', '2', '2017-09-18 10:05:20', '2017-09-18 10:05:23');
-
--- ----------------------------
 -- Table structure for product_info
 -- ----------------------------
 DROP TABLE IF EXISTS `product_info`;
@@ -88,7 +74,7 @@ CREATE TABLE `product_info` (
   `product_id` varchar(32) NOT NULL COMMENT '商品id',
   `product_name` varchar(64) NOT NULL COMMENT '商品名称',
   `product_price` double(8,2) DEFAULT NULL COMMENT '单价',
-  `product_stock` int(11) NOT NULL COMMENT '库存',
+  `sell_count` int(11) NOT NULL COMMENT '库存',
   `product_description` varchar(64) DEFAULT NULL COMMENT '描述',
   `product_icon` varchar(512) DEFAULT NULL COMMENT '小图',
   `category_id` int(11) NOT NULL COMMENT '类目编号',
@@ -98,5 +84,54 @@ CREATE TABLE `product_info` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='商品信息表';
 
 -- ----------------------------
--- Records of product_info
+-- Table structure for user
 -- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `age` int(11) DEFAULT NULL COMMENT '年龄',
+  `name` varchar(50) DEFAULT NULL COMMENT '名字',
+  `phone` varchar(50) NOT NULL COMMENT '手机号',
+  `password` varchar(50) DEFAULT NULL COMMENT '密码',
+  `last_login_time` datetime DEFAULT NULL COMMENT '最后登陆时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for user_check_count
+-- ----------------------------
+DROP TABLE IF EXISTS `user_check_count`;
+CREATE TABLE `user_check_count` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL COMMENT '关联的用户id',
+  `check_time` datetime NOT NULL COMMENT '签到时间',
+  `continue_count` int(11) NOT NULL COMMENT '连续签到天数',
+  `max_count` int(11) NOT NULL COMMENT '最长连续签到天数',
+  `history` bigint(11) NOT NULL COMMENT '签到历史记录',
+  `sum` int(11) NOT NULL COMMENT '总的签到天数',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for user_point
+-- ----------------------------
+DROP TABLE IF EXISTS `user_point`;
+CREATE TABLE `user_point` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL COMMENT '用户id',
+  `point` int(11) DEFAULT NULL COMMENT '积分',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for user_point_record
+-- ----------------------------
+DROP TABLE IF EXISTS `user_point_record`;
+CREATE TABLE `user_point_record` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL COMMENT '用户id',
+  `type` varchar(11) DEFAULT NULL COMMENT '积分类型',
+  `gain_point` int(11) DEFAULT NULL COMMENT '获得的积分',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
